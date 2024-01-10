d85b83fb08958271a4bb3c8e818161e4ddca4f05
Scott Pak <gpak86@gmail.com>
2024-01-09 20:36:37 -0500

    adding footer and card to react

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <p className="read-the-docs">
+            {/* Comment*/}
+            Scott Click on Vite
+        </p>  
+    );
+}
+
+export default Footer;
